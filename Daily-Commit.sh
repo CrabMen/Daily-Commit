f40cde1a263ab2dd5c88714cd87ad97927f92b0e
CrabMan <tobecrabman@163.com>
2019-05-08 23:20:18 +0800

    no commit message

--- Daily-Commit.sh
+++ Daily-Commit.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-current_dir=`pwd`
-echo "当前shell所在的文件路径：$current_dir"
+current_path=$(dirname $0)
 
-txt_dir="$current_dir"/Daily-Commit.txt
+cd ./${work_path}
+
+current_path=$(pwd)
+
+echo "当前shell所在的文件路径：$current_path"
+
+txt_dir="$current_path"/Daily-Commit.txt
 
 echo "当前Daily-Commit.txt所在的文件路径：$txt_dir"
 
 echo "commit" >> $txt_dir
 
-cd $current_dir
+cd $current_path
 
 git add .
 
