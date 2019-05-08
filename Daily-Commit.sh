36dd0f025f16fee2bdac9f333297dd6849efd5dd
CrabMan <tobecrabman@163.com>
2019-05-08 20:41:30 +0800

    修改sh文件

--- Daily-Commit.sh
+++ Daily-Commit.sh
@@ -4,6 +4,9 @@ cd /Users/zhijieios/CMProject/Daily-Commit
 echo "cd /Users/zhijieios/CMProject/Daily-Commit" >> /Users/zhijieios/CMProject/Daily-Commit/Daily-Commit.txt
 
 git add .
+
 git commit -m "no commit message"
+
 git push origin master
+
 
