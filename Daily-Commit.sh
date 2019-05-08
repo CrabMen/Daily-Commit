ed299db8fc510a9240ec87b5a88a24c5b212eccf
CrabMan <tobecrabman@163.com>
2019-05-08 23:17:45 +0800

    no commit message

--- Daily-Commit.sh
+++ Daily-Commit.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-echo 'dirname $0'
-echo "commit" >> /Users/zhijieios/CMProject/Daily-Commit/Daily-Commit.txt
-cd /Users/zhijieios/CMProject/Daily-Commit
+
+current_dir=`pwd`
+echo "当前shell所在的文件路径：$current_dir"
+
+txt_dir="$current_dir"/Daily-Commit.txt
+
+echo "当前Daily-Commit.txt所在的文件路径：$txt_dir"
+
+echo "commit" >> $txt_dir
+
+cd $current_dir
 
 git add .
 
 git commit -m "no commit message"
 
-git push 
+git push origin master
 
 
