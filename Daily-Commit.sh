c899a5879581bfc87a839b3b2a82b030940fe3d0
CrabMan <tobecrabman@163.com>
2019-05-08 20:33:26 +0800

    no commit message

--- Daily-Commit.sh
+++ Daily-Commit.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "commit" >> /Users/zhijieios/CMProject/Daily-Commit/Daily-Commit.txt
 cd /Users/zhijieios/CMProject/Daily-Commit
+echo "cd /Users/zhijieios/CMProject/Daily-Commit" >> /Users/zhijieios/CMProject/Daily-Commit/Daily-Commit.txt
+
 git add .
 git commit -m "no commit message"
 git push origin master
