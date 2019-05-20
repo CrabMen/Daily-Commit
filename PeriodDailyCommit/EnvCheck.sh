33e8086deb1cc03cfe541dfb34d97e4d540c0244
CrabMen <tobecrabman@163.com>
2019-05-20 20:26:47 +0800

    使用vscode测试代码

--- PeriodDailyCommit/EnvCheck.sh
+++ PeriodDailyCommit/EnvCheck.sh
@@ -17,11 +17,24 @@
 #cd $current_path
 
 #使用正则过滤年月日判断正确
-read -p "请输入起始日期（例如20160927）：" start_date
+#read -p "请输入起始日期（例如20160927）：" start_date
+#
+#echo "您输入的其实日期为 $start_date"
+#
+#read -p "请输入截止日期（例如20160927）：" end_date
+#
+#echo "请输入的截止日期为 $end_date"
 
-echo "您输入的其实日期为 $start_date"
+# cm_gdateInstalledCheck() {
+    gdate_dir=`which gdate`
+    echo "当前gdate的安装路径：$gdate_dir"
+    
+    
+    
+    
+    
+    gdate_dir_all_files=`ls -al $gdate_dir`
+    echo "当前gdate的安装路径下的所有文件：$gdate_dir_all_files"
 
-read -p "请输入截止日期（例如20160927）：" end_date
 
-echo "请输入的截止日期为 $end_date"
-
+# }
